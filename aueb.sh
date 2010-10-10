cf3cd01f75962016612faa3f7e815583ce5fa329
Ivan Kanakarakis <ivan.kanak@gmail.com>
2010-10-10 21:54:28 +0300

    using associative arrays, fixed variable quoting, introducing distro argument

--- aueb.sh
+++ aueb.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # {{{ 
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE" (Revision 42):
@@ -17,46 +17,53 @@
 # }}}
 
 # TODO
-# check needed progs (eg extract) at each stage
+# check needed progs at each stage
 # build pkgs func
 # print status mesg and keep log
-# dstro arg
-# PMS install/remove arg
 # profit!
 
- # Environment vars {{{
-BUILD_DIR="/tmp/src/"
+# Environment vars {{{
+PREFIX="/tmp/aueb"
+LOG="${PREFIX}/aueb.log"
+BUILD_DIR="${PREFIX}/aueb_pkg_src"
 BUILD_FLAGS="--prefix=/usr LDFLAGS=\"-Wl,--no-as-needed\""
 
-# Log files
-WPAERR="/tmp/wpa_aueb.err"
-WPALOG="/tmp/wpa_aueb.log"
+# wigi log files
+WPAERR="${PREFIX}/wpa_aueb.err"
+WPALOG="${PREFIX}/wpa_aueb.log"
 WPACONF="/etc/wpa_supplicant/wpa_aueb.conf"
 
-# Package Managers
-PMS=(	[ubuntu]="apt-get install"
+# Package Managers installation/removal of apps
+declare -A PMSI
+PMSI=(	[ubuntu]="apt-get install"
 		[arch]="pacman -S" )
+declare -A PMSR
+PMSR=(	[ubuntu]="apt-get remove"
+		[arch]="pacman -Rs" )
 
 # Applications throught the PMS
+declare -A APPS
 APPS=(	[common]="netbeans scilab geany nmap spim tcsh wireshark"
-		[ubuntu]="openjdk-6-jdk build-essential"
-		[arch]="openjdk6 base-devel" )
+		[ubuntu]="openjdk-6-jdk build-essential wireless-tools wpasupplicant"
+		[arch]="openjdk6 base-devel wireless_tools wpa_supplicant" )
 
- # }}}
-
-# FIXME Applications that need to be built from source {{{
-LAMPP="http://www.apachefriends.org/download.php?xampp-linux-1.7.3a.tar.gz"
-OMNET="http://www.omnetpp.org/omnetpp/doc_download/2217-omnet-41-source--ide-tgz"
-# ACCEPT THE AGREEMENT
-NESSUS="http://www.nessus.org/download/index.php?product=nessus42-linux"
-# register to download Quartus
-QUARTUS="https://www.altera.com/servlets/download3?swcode=WWW-SWD-QII-WE-100SP1-LNX&l=en"
 # }}}
 
-# Usage message {{{
+# FIXME crete arch-like vars for each package, each
+# FIXME parckege will be a function that resets the vars (url/pkgname/etc)
+# Applications that need to be built from source {{{
+#LAMPP="http://www.apachefriends.org/download.php?xampp-linux-1.7.3a.tar.gz"
+#OMNET="http://www.omnetpp.org/omnetpp/doc_download/2217-omnet-41-source--ide-tgz"
+# ACCEPT THE AGREEMENT
+#NESSUS="http://www.nessus.org/download/index.php?product=nessus42-linux"
+# register to download Quartus
+#QUARTUS="https://www.altera.com/servlets/download3?swcode=WWW-SWD-QII-WE-100SP1-LNX&l=en"
+# }}}
+
+# Usage message {{{ 
 function usage() {
 cat << EOF
-usage: $(basename $0) [option [suboption]]
+usage: $(basename "$0") option suboption distro
 
 	Options are:
 	apps [app_opts]			application management
@@ -69,26 +76,29 @@ usage: $(basename $0) [option [suboption]]
 	Wifi options are:
 	connect				connect to wireless
 	disconnect			disconnect from wireless
+
+	distro				the linux distribution [supported:ubuntu,arch]
 EOF
+exit 1
 }
 # }}}
 
 # Applications management {{{
 # Extract an archive
 function extract() {
-	if [ -f $1 ] ; then
-		case $1 in
-			*.tar.bz2)   tar xvjf $1    ;;
-			*.tar.gz)    tar xvzf $1    ;;
-			*.bz2)       bunzip2 $1     ;;
-			*.rar)       unrar x $1     ;;
-			*.gz)        gunzip $1      ;;
-			*.tar)       tar xvf $1     ;;
-			*.tbz2)      tar xvjf $1    ;;
-			*.tgz)       tar xvzf $1    ;;
-			*.zip)       unzip $1       ;;
-			*.Z)         uncompress $1  ;;
-			*.7z)        7z x $1        ;;
+	if [ -f "$1" ] ; then
+		case "$1" in
+			*.tar.bz2)   tar xvjf ""$1""    ;;
+			*.tar.gz)    tar xvzf "$1"    ;;
+			*.bz2)       bunzip2 "$1"     ;;
+			*.rar)       unrar x "$1"     ;;
+			*.gz)        gunzip "$1"      ;;
+			*.tar)       tar xvf "$1"     ;;
+			*.tbz2)      tar xvjf "$1"    ;;
+			*.tgz)       tar xvzf "$1"    ;;
+			*.zip)       unzip "$1"       ;;
+			*.Z)         uncompress "$1"  ;;
+			*.7z)        7z x "$1"        ;;
 			*)           echo "I don't know how to extract \"$1\"..." ;;
 		esac
 	else
@@ -96,65 +106,81 @@ function extract() {
 	fi
 }
 
-# FIXME Build a package from source
-function buildpkgs() {
-	mkdir -p $BUILD_DIR
-	cd $BUILD_DIR
-	wget $pkg_url
-	extract $pkg
-	./configure --${CONF_FLAGS}
-	make && make install
+# empty build directory
+function cleanbuild() {
+	rm -r $BUILD_DIR/*
 }
 
-# FIXME for loop..
-# Install a package using PMS
+# FIXME Build a package from source
+function buildpkgs() {
+	wget "$pkg_url"
+	extract "$pkg"
+	./configure --${CONF_FLAGS}
+	make && make install
+	cleanbuild
+}
+
+# Install  packages using PMS
 function installpkgs() {
-	${PMS[$distro]} ${APPS["common"]} ${APPS[$distro]}
+	${PMSI["$DISTRO"]} ${APPS["common"]} ${APPS["$DISTRO"]}
+	(( $? )) && echo Failure || echo Success
 }
 
 # main install function
 function install() {
 	installpkgs
+	[ -e "$BUILD_DIR" ] || mkdir -p $BUILD_DIR
+	local curpath="$PWD"
+	cd $BUILD_DIR
 	buildpkgs
+	cd "$curpath"
+}
+
+# remove packages using PMS
+function removepkgs() {
+	${PMSR["$DISTRO"]} ${APPS["common"]} ${APPS["$DISTRO"]}
+	(( $? )) && echo Failure || echo Success
 }
 
 # main remove function
 function remove() { 
-	echo cookies
+	removepkgs
+	removebuildpkgs
 }
 
 # manage application installation
 function apps() {
-	case $1 in
+	case "$1" in
 		install)	install	;;
 		remove)		remove	;;
-		*)			usage && exit 1	;;
+		*)			usage	;;
 	esac
 }
 # }}}
 
+# FIXME is a network manager running? which one? how to stop/restart it? 
 # wifi management {{{
 # connect to wifi
 function connectwifi() {
 	wpa_wifi
 	/etc/{rc,init}.d/{networkmanager,wicd} stop &>/dev/null
-	ifconfig $IFACE down
-	ifconfig $IFACE up
-	wpa_supplicant -Dwext -i$IFACE -c $WPACONF 1>$WPALOG 2>$WPAERR
+	ifconfig "$IFACE" down
+	ifconfig "$IFACE" up
+	wpa_supplicant -Dwext -i "$IFACE" -c "$WPACONF" 1>"$WPALOG" 2>"$WPAERR"
 }
 
 # disconnect from wifi
 function disconnectwifi() {
-	ifconfig $IFACE down
+	ifconfig "$IFACE" down
 	/etc/{rc,init}.d/{networkmanager,wicd} start &>/dev/null
 }
 
 # create wpa_supplicant configuration file
 function wpa_wifi() {
-	[[ -e $WPACONF ]] && return
-	mkdir -p $(dirname ${WPACONF})
-	cat > $WPACONF << EOF
-ctrl_interface=/tmp/wpa_aueb
+	[ -e "$WPACONF" ] && return
+	mkdir -p "$(dirname ${WPACONF})"
+	cat > "$WPACONF" << EOF
+ctrl_interface=${PREFIX}/wpa_aueb
 eapol_version=1
 ap_scan=1
 fast_reauth=1
@@ -172,19 +198,22 @@ EOF
 
 # manage wifi connection
 function wifi() {
-	case $1 in
+	case "$1" in
 		connect)	connectwifi		;;
 		disconnect)	disconnectwifii	;;
-		*)			usage && exit 1	;;
+		*)			usage			;;
 	esac
 	IFACE="$(iwconfig 2>&1 | grep -v "no\|^$" | head -1 | awk '{ print $1 }')"
 }
 # }}}  
 
 # Main run 
-case $1 in 
-	wifi) wifi $2 ;;
-	apps) apps $2 ;;
+[ -n "$3" ] && DISTRO="$3" || usage
+[ "$DISTRO" == "ubuntu" -o "$DISTRO" == "arch" ] || usage
+mkdir -p "$PREFIX"
+case "$1" in 
+	wifi) wifi "$2" ;;
+	apps) apps "$2" ;;
 	*) usage
 esac
 
