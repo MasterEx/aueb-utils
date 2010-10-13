aa0353d89b582b1c963d83cd3a5338c63b787af8
Periklis Ntanasis <pntanasis@gmail.com>
2010-10-13 19:27:10 +0300

    quartus pkg was added - legal notice may need fix

--- aueb.sh
+++ aueb.sh
@@ -99,9 +99,64 @@ function xampp() {
 	pkgrel="1.7.3a"
 	pkgdesc="The Linux version of XAMPP"
 	arch=('i686' 'x86_64')
-	url="http://www.apachefriends.org/download.php?xampp-linux-1.7.3a.tar.gz"
+	url="http://www.apachefriends.org/en/xampp-linux.html"
 	source=(${FOSS_AUEB}sources/${pkgname}-linux-${pkgver}.tar.gz)
 	md5sum=('89c13779cf6f0925d5c1c400d31a1cc3')
+}
+
+# ALTERA LICENSE TO COPY INFORMATION {{{
+#
+#You are licensed to download and copy documentation, software, and other
+# materials from this website (including the myAltera and Self-Service 
+#Licensing Center portions of this website) provided you agree to the 
+#following terms and conditions:
+#
+#You may use the Materials for informational, non-commercial purposes only.
+#You may not alter or modify the Materials in any way.
+#You may not use any graphics separate from any accompanying text.
+#You may distribute copies of the documentation available at this website 
+#only to customers and potential customers of Altera® products. However, 
+#you may not charge them for such use. Any other distribution to third parties 
+#is prohibited unless you obtain the prior written consent of Altera.
+#You may use any software provided on this website provided that you agree 
+#to be bound by the terms and conditions of Altera's Program Subscription 
+#License Agreement or other applicable license agreement. Unless expressly 
+#permitted, you may not modify, reverse engineer, or disassemble any software.
+#You may not install any software that is accompanied by or includes a 
+#License Agreement unless you first have agreed to the License Agreement terms. 
+#If no end user License Agreement accompanies or is included with the software, 
+#then such software shall be deemed to be Materials hereunder and this Legal 
+#Notice shall govern your use of such software. FURTHER REPRODUCTION OR DISTRIBUTION 
+#OF ANY SOFTWARE IS EXPRESSLY PROHIBITED, UNLESS SUCH REPRODUCTION OR DISTRIBUTION 
+#IS EXPRESSLY PERMITTED BY THE LICENSE AGREEMENT ACCOMPANYING OR INCLUDED WITH SUCH SOFTWARE.
+#You may not use the Materials in any way that may be adverse to Altera´s interests.
+#You may not use this website (including, without limitation, any software, 
+#documentation, or other Materials you may obtain through your use of this website) 
+#(1) in a manner that violates any local, state, national, foreign or 
+#international statutes, regulations, rules, orders, treaties, or other laws, 
+#(2) to interfere with or disrupt the operation of the website or servers 
+#or networks connected to the website, or (3) attempt to gain unauthorized 
+#access to any portion of the website or any other accounts, computer systems, 
+#servers, or networks connected to the website, whether through hacking, 
+#password mining, or any other means.
+#All copies of materials that you download or copy from this website must 
+#include a copy of this Legal Notice.
+#
+#Failure to comply with these terms and conditions will terminate the license.
+# }}}
+function quartus() {
+	pkgname="quartus"
+	pkgver="10.0sp1"
+	pkgrel="10.0"
+	pkgdesc="Quartus II Web Edition Software Version 10.0 Service Pack 1 for Linux"
+	arch=('i686' 'x86_64')
+	url="https://www.altera.com/download/software/quartus-ii-we"
+	legalurl="http://www.altera.com/common/legal.html"
+	legalnotice="This is NOT FOSSoftware Please visit \
+	 $legalurl before proceeding.\
+	If you proceed it means that you understand and agree with the above legal notice"
+	source=(${FOSS_AUEB}sources/${pkgname}-linux-${pkgver}.sh)
+	md5sum=('eab8466927e83c38d2a449842d3f372d')
 }
 
 # ACCEPT THE AGREEMENT
